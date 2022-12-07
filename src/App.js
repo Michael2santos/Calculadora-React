a923fee904a9edf792daf0146b2e1d27e5fdf92c
Michael <michael2msp@gmail.com>
2022-12-07 15:16:36 -0300

    Commit do projeto calculadora desenvolvida com react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container, Content, Row} from './styles';
+import Input from './components/Input';
+import Button from './components/Button';
+import { useState } from 'react';
 
-function App() {
+const App = () =>  {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  const handleSumNumbers = () => {//operação de soma
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('+')
+    }else{
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+  }
+
+  const handleMinusNumbers = () => {//operação de subtração
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('-')
+    }else{
+      const sum = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+  }
+
+  const handleMultiNumbers = () => {//operação de multiplicação
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('*')
+    }else{
+      const sum = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+  }
+
+  const handleDivNumbers = () => {//operação de divisão
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0')
+      setOperation('/')
+    }else{
+      const sum = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('')
+    }
+  }
+
+  const handleEquals = () => {//setando operação e mostrando resultado
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
+      switch(operation){
+        case '+':
+            handleSumNumbers();  
+          break;
+        case '-':
+            handleMinusNumbers();  
+          break;
+        case '*':
+            handleMultiNumbers();  
+          break;
+        case '/':
+            handleDivNumbers();  
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  const handleOnClear = () =>{//limpar dados do input
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('')
+  }
+
+  const handleAddNumber = (number) =>{//adicionar dados no input
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber('0')}/>
+          <Button label="c" onClick={handleOnClear}/>
+          <Button label="/" onClick={handleDivNumbers} />
+          <Button label="x" onClick={handleMultiNumbers}/>
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')}/>
+          <Button label="8" onClick={() => handleAddNumber('8')}/>
+          <Button label="9" onClick={() => handleAddNumber('9')}/>
+          <Button label="-" onClick={handleMinusNumbers}/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="+" onClick={handleSumNumbers}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="=" onClick={handleEquals}/>
+        </Row>
+        
+      </Content>
+    </Container>
   );
 }
 
